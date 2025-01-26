a6a9b0ca54a073ed811cc786052441493e8233fa
Lucas Sallum <lucas.sallum@gmail.com>
2025-01-26 09:35:31 -0300

    Initial Spring Structure

---
+++ src/test/java/com/shopcore/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopcore.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
